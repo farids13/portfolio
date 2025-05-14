8542d1b42d629bce40712eaad2f935c504669a61
satria <faridsatria24@gmail.com>
2025-05-14 13:54:29 +0700

    fixing implement react

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,47 +1,47 @@
 import Image from "next/image";
 import { ReactElement } from "react";
 
-export default function Home() :ReactElement {
+export default function Home(): ReactElement {
   return (
-   <div>
+    <div>
       <header className="flex justify-between items-center m-20 font-bold text-2xl">
         <Image src="/images/logo.png" alt="logo" width={100} height={100} />
 
         <div className="flex gap-20">
-          <a href="">ABOUT</a>
-          <a href="">SKILLS</a>
-          <a href="">PROJECTS</a>
+          <a href="#">ABOUT</a>
+          <a href="#">SKILLS</a>
+          <a href="#">PROJECTS</a>
         </div>
 
-        
+
         <button
-      className="
+          className="
         flex items-center border border-white rounded-full px-5 py-2
         bg-transparent text-white relative group transition-all
         hover:border-lime-300
       "
-      style={{ borderColor: "#fff" }}
-    >
-      <span className="mr-4">CONTACT</span>
-      <span
-        className="
+          style={{ borderColor: "#fff" }}
+        >
+          <span className="mr-4">CONTACT</span>
+          <span
+            className="
           flex items-center justify-center
           w-8 h-8 rounded-full
           bg-lime-200
           group-hover:bg-lime-300
           transition-colors
         "
-      >
-        {/* <ArrowUpRight size={20} color="#222" /> */}
-      </span>
-    </button>
-        
+          >
+            {/* <ArrowUpRight size={20} color="#222" /> */}
+          </span>
+        </button>
+
       </header>
       <main>
         <section className="hero">
           <div className="flex flex-col items-center gap-2 text-6xl">
             <div className="flex gap-2 justify-center items-center">
-              <h1>I'm</h1>
+              <h1>I&apos;m</h1>
               <h1 className="text-lime-200">Farid Satria</h1>
             </div>
             <div className="flex gap-2 justify-center items-center">
@@ -55,8 +55,8 @@ export default function Home() :ReactElement {
         </section>
       </main>
       <footer>
-        <p>Farid's Portfolio</p>
+        <p>Farid&apos;s Portfolio</p>
       </footer>
-   </div>
+    </div>
   );
 }
