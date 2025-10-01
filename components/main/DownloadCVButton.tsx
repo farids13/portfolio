9a770640b3ac7026a9b418eeb9127eacc8cc917b
Farid Satria <faridsatria24@gmail.com>
2025-10-01 19:04:23 +0700

    Dev: add link for download cv

--- components/main/DownloadCVButton.tsx
+++ components/main/DownloadCVButton.tsx
@@ -4,7 +4,7 @@ import React from "react";
 import Button from "../ui/Button";
 
 const DownloadCVButton = () => {
-  return <Button className="w-1/2 py-5 mt-10 " onClick={() => console.log('Download CV')}>DOWNLOAD MY CV</Button>;
+  return <Button className="w-1/2 py-5 mt-10 " onClick={() => window.open('https://drive.google.com/drive/folders/1fkEb2hWAqIyl_Rjfvsdcz7bNGrXpxaE1?usp=sharing')}>DOWNLOAD MY CV</Button>;
 };
 
 export default DownloadCVButton;
